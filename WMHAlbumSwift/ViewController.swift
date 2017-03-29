cfa5fb40325126719b74d7d5a3cd28a74c6e318e
Archer <minhang.wang@icloud.com>
2017-03-29 10:23:58 +0800

    Initial Commit

---
+++ WMHAlbumSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WMHAlbumSwift
+//
+//  Created by Archer on 2017/3/29.
+//  Copyright © 2017年 jiuji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
