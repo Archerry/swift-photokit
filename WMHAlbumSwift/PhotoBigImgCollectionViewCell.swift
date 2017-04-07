461a372525d48f4a4a2f02876305c3ac565a0d27
Archer <minhang.wang@icloud.com>
2017-04-07 15:17:19 +0800

    first commit

---
+++ WMHAlbumSwift/PhotoBigImgCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoBigImgCollectionViewCell.swift
+//  WMHAlbumSwift
+//
+//  Created by Archer on 2017/4/1.
+//  Copyright © 2017年 jiuji. All rights reserved.
+//
+
+import UIKit
+
+class PhotoBigImgCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ WMHAlbumSwift/PhotoListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoListCollectionViewCell.swift
+//  WMHAlbumSwift
+//
+//  Created by Archer on 2017/3/31.
+//  Copyright © 2017年 jiuji. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListCollectionViewCell: UICollectionViewCell {
+    
+}
